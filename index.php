fad2862e9a3b17c809a0e3d1659919472d9dd946
Namcha <Lanend18@gmail.com>
2023-05-04 23:18:40 +0700

    For-Continue

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 //http://localhost:8255/colorful09af/index.php
-for ($i = 1;$i <= 8 ;$i++){
-    echo "<p>$i</p>";
+for ($i = 10;$i <= 20 ;$i++){
+    if ($i%3 != 0) {
+        echo "<p>$i</p>";
+    }
+    else {
+        continue;
+        echo "เลข $i หาร 3 ลงตัว";
+    }
 }
 ?>
