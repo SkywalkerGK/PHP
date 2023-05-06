7ed5d2c094e58cafc2c72bdb3c3353d2eaddc89a
Namcha <Lanend18@gmail.com>
2023-05-07 00:49:57 +0700

    Form-HTML

--- index.php
+++ index.php
@@ -1,20 +1,38 @@
-<?php
+<?php 
 //http://localhost:8255/colorful09af/index.php
+require("helpers/db.php"); 
 ?>
 
-<?php
-require("helpers/db.php");
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Colorful 09AF</title>
+</head>
+<body>
+    <div>
+        <?php
+            $sql = "SELECT * FROM color";
+            $result = mysqli_query($connection,$sql);
+            $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        ?>
+        <h3>พบสี <?php echo count($rows); ?> รายการ</h3>
+        <?php foreach ($rows as $row): ?>
+            <div>
+                <h4><?php echo $row["title"]; ?></h4>
+                <p><?php echo $row["code"]; ?></p>
+            </div>
 
-$sql = "SELECT * FROM color";
-$result = mysqli_query($connection,$sql);
-$rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-foreach ($rows as $row) {
-    var_dump($row);
-    echo  "<h4>$row[title]</h4>";
-    echo  "<p>$row[code]</p>";
-}
+        <?php endforeach; ?>
+
+    </div>
+</body>
+</html>
+
+<?php //mysqli_close($connection); ?>
 
 
-mysqli_close($connection);
 
