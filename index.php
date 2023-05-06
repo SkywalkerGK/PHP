4a49f658c78844a201955438059b477713f83505
Namcha <Lanend18@gmail.com>
2023-05-06 23:26:30 +0700

    Arrray-DisplayAll2

--- index.php
+++ index.php
@@ -10,25 +10,25 @@ $foods = [
         "title" => "อาหารแมวน้อย",
         "price" => 299
     ],
-
     [
         "title" => "อาหารหมา",
         "price" => 399
     ]
-
 ];
 
 echo $foods[0]["title"];
 
-//for ($i=0;$i < count($foods);$i++) {
-//    $food = $foods[$i];
+//foreach ($foods as $food) {
 //    echo "<h2>$food[title]</h2>";
 //    echo "<p>$food[price]</p>";
 //}
 
-foreach ($foods as $food) {
-    echo "<h2>$food[title]</h2>";
-    echo "<p>$food[price]</p>";
-}
-
 ?>
+
+<?php foreach ($foods as $food): ?>
+    <div>
+        <h2> <?php echo $food["title"]; ?> </h2>
+        <p>ราคา = <?php echo $food["price"] ?> บาท</p>
+    </div>
+<?php endforeach; ?>
+
