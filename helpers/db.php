24a32ac2ea6275ae344122d89888e9ecdcd6c654
Namcha <Lanend18@gmail.com>
2023-05-07 00:27:52 +0700

    Query_Data

--- helpers/db.php
+++ helpers/db.php
@@ -8,9 +8,9 @@ $port = 3306;
 mysqli_report(MYSQLI_REPORT_OFF);
 $connection = mysqli_connect($hostname,$username,$password,$database,$port);
 
-if (!$connection) {
-    die("ทอดสะพานไม่ได้" . mysqli_connect_error());
-}
-else {
-    echo "ทอดสะพานได้แล้ว";
-}
+//if (!$connection) {
+//    die("ทอดสะพานไม่ได้" . mysqli_connect_error());
+//}
+//else {
+//    echo "ทอดสะพานได้แล้ว";
+//}
--- index.php
+++ index.php
@@ -5,5 +5,16 @@
 <?php
 require("helpers/db.php");
 
+$sql = "SELECT * FROM color";
+$result = mysqli_query($connection,$sql);
+$rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+foreach ($rows as $row) {
+    var_dump($row);
+    echo  "<h4>$row[title]</h4>";
+    echo  "<p>$row[code]</p>";
+}
+
+
 mysqli_close($connection);
 
