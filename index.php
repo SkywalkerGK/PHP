3e49b0489dfb3d79a541ab3c7577b1eb1d402c24
Namcha <Lanend18@gmail.com>
2023-05-07 12:37:48 +0700

    GET-2

--- index.php
+++ index.php
@@ -3,13 +3,22 @@
 require("helpers/db.php"); 
 ?>
 
+<?php
+$searchTitle = "";
+$searchValue = "";
+if (isset($_GET["search"])) {
+    $searchTitle = "ค้นหา \"$_GET[search]\" | ";
+    $searchValue = $_GET["search"];
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Colorful 09AF</title>
+    <title><?php echo $searchTitle; ?> Colorful 09AF</title>
 </head>
 <body>
     <div>
@@ -18,7 +27,7 @@ require("helpers/db.php");
              
             $sql = "SELECT * FROM color ";
             if (isset($_GET["search"])) {
-                $search = $_GET["search"];
+                $search = mysqli_real_escape_string($connection,$_GET["search"]);
                 $sql .= "WHERE title LIKE '%$search%' ";
             }
             $sql .= "ORDER BY id DESC";
@@ -27,7 +36,7 @@ require("helpers/db.php");
         ?>
         <form>
             <p>
-                <input type="search" name="search">
+                <input type="search" name="search" value="<?php echo $searchValue; ?>">
                 <button type="submit">ค้นหา</button>
             </p>
         </form>
